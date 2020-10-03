8cf7435ceb7660828ce97586888c1df220aeadd3
Matti Uhlig <matti.uhlig@yahoo.de>
2020-10-03 17:28:49 +0200

    After save commit

---
+++ Assets/Orientation.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Orientation : MonoBehaviour
+{
+
+    [SerializeField] Transform center;
+    [SerializeField] Transform north;
+    [SerializeField] Transform east;
+    [SerializeField] Transform south;
+    [SerializeField] Transform west;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
