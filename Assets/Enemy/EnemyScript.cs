3d417fe3e5ea196d3b2f7a6bd41f49af498a3c7a
Matti Uhlig <matti.uhlig@yahoo.de>
2020-10-03 15:42:04 +0200

    Basically added listening

---
+++ Assets/Enemy/EnemyScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+
+    [SerializeField] AudioSource myNoise;
+
+    bool makingNoise = false;
+
+    public void SetListeningState(bool state)
+    {
+
+        makingNoise = state;
+        if(state)
+        {
+
+            myNoise.mute = false;
+        }
+        else
+        {
+
+            myNoise.mute = true;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        myNoise.mute = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Player/ListeningScript.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+
+public class ListeningScript : MonoBehaviour
+{
+
+    [SerializeField] GameObject[] enemies;
+    [SerializeField] float maxDist = 10f;
+
+    [Space]
+
+    [SerializeField] LayerMask raycastLayer;
+    [SerializeField] Transform cam;
+
+    bool listening = false;
+    bool wasListening = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        listening = false;
+
+        if(Input.GetButton("Fire1"))
+        {
+
+            if (Physics.Raycast(cam.position, cam.forward, out RaycastHit hit, maxDist, raycastLayer))
+            {
+
+                //We hit a button and now want to start listening from the new position
+                if(hit.collider.CompareTag("Button"))
+                {
+
+                    listening = true;
+                    TellEnemiesThatWeAreListening(true);
+                    Debug.Log("Started Listening");
+                }
+            }
+        }
+
+        if (!listening)
+        {
+
+            NotListening();
+        }
+
+
+
+        wasListening = listening;
+    }
+
+    void NotListening()
+    {
+
+        //Stop listening
+        if (wasListening)
+        {
+
+            listening = false;
+            TellEnemiesThatWeAreListening(false);
+            Debug.Log("Stopped listening");
+        }
+    }
+
+    void TellEnemiesThatWeAreListening(bool state)
+    {
+
+        foreach(GameObject go in enemies)
+        {
+
+            go.GetComponent<EnemyScript>().SetListeningState(state);
+        }
+    }
+}
